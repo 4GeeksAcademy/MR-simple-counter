e995b6c167d428ab62582bba8e5b85f7419aa186
Matias Rivas <mat500.600@gmail.com>
2023-10-31 17:42:27 -0300

    generado contenedores y estilos, en proceso de hacer el contador

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import PropTypes from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const Home = (props) => {
+  const [Counter, setCounter] = useState(0);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((previousCounter) => previousCounter + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return (
+    <>
+      <div className="banner py-5 px-5">
+        <div className="container mx-1 reloj">
+          <FontAwesomeIcon icon={faClock} />
+        </div>
+        <div className="container  mx-1  unidad">{props.unidad}</div>
+        <div className="container  mx-1 decena">{props.decena}</div>
+        <div className="container  mx-1 centena">{props.centena}</div>
+        <div className="container  mx-1 unidadmil">{props.unidadmil}</div>
+        <div className="container  mx-1 decenamil">{props.decenamil}</div>
+        <div className="container  mx-1 centenamil">{props.centenamil}</div>
+      </div>
+    </>
+  );
+};
+
+Home.propTypes = {
+  unidad: PropTypes.number,
+  decena: PropTypes.number,
+  centena: PropTypes.number,
+  unidadmil: PropTypes.number,
+  decenamil: PropTypes.number,
+  centenamil: PropTypes.number,
 };
 
 export default Home;
