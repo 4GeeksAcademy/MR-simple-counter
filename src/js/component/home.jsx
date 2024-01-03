078842d6a99d4a5d4a70a27af7a56e5bf24822a7
Matias Rivas <mat500.600@gmail.com>
2024-01-03 19:21:00 -0300

    creado componente necesario y funcionalidad

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from "prop-types";
 
-const Home = (props) => {
+const Home = () => {
   const [Counter, setCounter] = useState(0);
 
   useEffect(() => {
@@ -15,18 +15,32 @@ const Home = (props) => {
       clearInterval(interval);
     };
   }, []);
+
+  const calculateTime = (totalSeconds) => {
+    const centenamil = Math.floor(totalSeconds / 100000) % 10;
+    const decenamil = Math.floor(totalSeconds / 10000) % 10;
+    const unidadmil = Math.floor(totalSeconds / 1000) % 10;
+    const centena = Math.floor(totalSeconds / 100) % 10;
+    const decena = Math.floor(totalSeconds / 10) % 10;
+    const unidad = totalSeconds % 10;
+
+    return { unidad, decena, centena, unidadmil, decenamil, centenamil };
+  };
+
+  const timeParts = calculateTime(Counter);
+
   return (
     <>
       <div className="banner py-5 px-5">
         <div className="container mx-1 reloj">
           <FontAwesomeIcon icon={faClock} />
         </div>
-        <div className="container  mx-1  unidad">{props.unidad}</div>
-        <div className="container  mx-1 decena">{props.decena}</div>
-        <div className="container  mx-1 centena">{props.centena}</div>
-        <div className="container  mx-1 unidadmil">{props.unidadmil}</div>
-        <div className="container  mx-1 decenamil">{props.decenamil}</div>
-        <div className="container  mx-1 centenamil">{props.centenamil}</div>
+        <div className="container mx-1 centenamil">{timeParts.centenamil}</div>
+        <div className="container mx-1 decenamil">{timeParts.decenamil}</div>
+        <div className="container mx-1 unidadmil">{timeParts.unidadmil}</div>
+        <div className="container mx-1 centena">{timeParts.centena}</div>
+        <div className="container mx-1 decena">{timeParts.decena}</div>
+        <div className="container mx-1 unidad">{timeParts.unidad}</div>
       </div>
     </>
   );
